a8f6038cbf75031716b7203103ceb74ff3d47f26
haruki-matsuzaki <matsuzaki@shibuya-ad.com>
2026-06-26 15:11:31 +0900

    Configure Firebase auth and rules

--- config.js
+++ config.js
@@ -3,9 +3,11 @@ window.ABC_TASK_AUTH_CONFIG = {
   dataSource: "firestore",
   firestoreAppPath: ["salesTaskApps", "abcClinic"],
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    appId: ""
+    apiKey: "AIzaSyC3e0DdGdUtwI_r2IjW0hbBadGyKQ-iPXY",
+    authDomain: "sales-task-2da87.firebaseapp.com",
+    projectId: "sales-task-2da87",
+    storageBucket: "sales-task-2da87.firebasestorage.app",
+    messagingSenderId: "53589406974",
+    appId: "1:53589406974:web:54bcf418bc04e13f0861af"
   }
 };
